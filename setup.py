7976fa3394270e8e0e4b7fb039615ca42c028c42
Vaidy Gopalakrishnan <vaidy.gopalakrishnan@solarwinds.com>
2016-01-07 09:53:19 -0600

    Bumped up version no in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="librato-python-web",
-    version="0.1.7",
+    version="0.2",
     description=("Librato Python Agent. Copyright (c) 2015 Librato, Inc "
                  "All Rights Reserved"),
 
