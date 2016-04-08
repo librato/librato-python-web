213c8532bd904ff7f1a7526062a8728878f606c0
Vaidy Gopalakrishnan <vaidy.gopalakrishnan@solarwinds.com>
2016-04-08 10:16:09 -0500

    Test: Add required module attrib for Heroku failure

--- librato_python_web/instrumentor/data/psycopg2.py
+++ librato_python_web/instrumentor/data/psycopg2.py
@@ -5,7 +5,7 @@ from librato_python_web.instrumentor.base_instrumentor import BaseInstrumentor, 
 
 
 class Psycopg2Instrumentor(BaseInstrumentor):
-    modules = {'psycopg2': []}
+    modules = {'psycopg2': ['connect']}
 
     def __init__(self):
         super(Psycopg2Instrumentor, self).__init__()
