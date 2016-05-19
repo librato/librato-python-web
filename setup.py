3d37a049a6f8785fa5932c06cd597e1ad485ddc3
Vaidy Gopalakrishnan <vaidy.gopalakrishnan@solarwinds.com>
2016-05-19 09:58:59 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="librato-python-web",
-    version="0.40.7",
+    version="0.50",
     description=("Librato Python Agent. Copyright (c) 2015 Librato, Inc "
                  "All Rights Reserved"),
 
